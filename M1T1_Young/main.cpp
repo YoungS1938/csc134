ab7a13e56795dc18d5fb9f22c48c1210beaa4de1
YoungS1938 <46724895+YoungS1938@users.noreply.github.com>
2019-01-15 14:19:00 -0500

    First upload

---
+++ M1T1_Young/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// CSC 134
+// M1T1
+// Seth Young
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello from CSC134!" << endl;
+    cout << "random text" << endl;
+    return 0;
+}
