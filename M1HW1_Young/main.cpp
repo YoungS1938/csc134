0bddc587763a50396d48752af9c97b24a5ad3924
YoungS1938 <46724895+YoungS1938@users.noreply.github.com>
2019-01-17 13:59:49 -0500

    M1HW1_Young
    Small introduction code.

---
+++ M1HW1_Young/main.cpp
@@ -0,0 +1,21 @@
+// CSC 134
+//M1HW1
+// Seth Young
+// 1/17/19
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // provide a brief introduction.
+    cout << "Hello, My name is Seth Young" << endl;
+    cout << "I started learning programming in " << 2018 << endl;
+    cout << " I do not have any pets" << endl;
+    cout << "I have " << 2 << " siblings" << endl;
+    cout << "\ta brother" << endl;
+    cout << "\ta half sister" << endl;
+    cout << endl;
+    cout << "testing\t1\t2\t3" << endl;
+    return 0;
+}
